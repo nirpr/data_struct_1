4a50caff023efc267ae06da43f74a162dfeb2c3a
shakedhl17 <shakedhl@mta.ac.il>
2023-05-02 17:06:27 +0300

    comments in main

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 
 int main()
 {
+	//create the arr that all the function will use
 	int arr[6] = { 1,9,7,8,9,15 };
-	printNumIfSum1(arr, 6, 16);
 
+	//first function - nested loop
+	printNumIfSum1(arr, 6, 16);
+	
+	//second function - recursion
+	//the y parameter set to 0
 	printNumIfSum2Rec(arr, 6, 16, 0);
+
+	//third function - recursion by stack
 	return 0;
 }
