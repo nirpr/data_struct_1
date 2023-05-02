80a0c75b82805c47d6c965f7a985d79847654f8e
shakedhl17 <shakedhl@mta.ac.il>
2023-05-02 22:52:05 +0300

    bug fix

--- utilities.cpp
+++ utilities.cpp
@@ -1,3 +1,4 @@
+
 #include "utilities.h"
 using namespace std;
 
@@ -102,12 +103,12 @@ void printNumIfSum3Stack(vector<int> arr, int n, int x, int y)
 		}
 		else if (curr.getPos() == itemType::AFTER_FIRST)
 		{
-			curr.setPos(itemType::AFTER_SECOND);
+			curr.setPos(itemType::AFTER_THIRD);
 			S.push(curr);
 			curr.sety(arr[curr.getn() - 1]);
 			curr.setn(curr.getn() - 1);
 			curr.setPos(itemType::START);
-			returnFromRec = false;
+			returnFromRec = true;
 		}
 		else if (curr.getPos() == itemType::AFTER_SECOND) {
 			curr.setPos(itemType::AFTER_THIRD);
